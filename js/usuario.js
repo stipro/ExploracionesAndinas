c417bb758778e8d3386d86d151e416d8e4855e0e
Frank Laptop Asus <stipro150197@gmail.com>
2022-02-18 11:36:00 -0500

    Arreglando modulo de usuarios

--- js/usuario.js
+++ js/usuario.js
@@ -2,6 +2,48 @@
 const btnAgregar = document.getElementById("btn-Agregar");
 const btnInsert = document.getElementById("mbtn-insert");
 
+
+const iptInsert_nombrecolaborador_Usuario = document.getElementById("ipt-insert-nombreColaborador-usuario");
+const iptInsert_dnicolaborador_Usuario = document.getElementById("ipt-insert-dniColaborador-usuario");
+const iptInsert_nombre_Usuario = document.getElementById("ipt-insert-nombre-usuario");
+const iptInsert_clave_Usuario = document.getElementById("ipt-insert-clave-usuario");
+const iptInsert_correo_Usuario = document.getElementById("ipt-insert-correo-usuario");
+const iptInsert_estado_Usuario = document.getElementById("ipt-insert-estado-usuario")
+
+btnAgregar.addEventListener("click", () => {
+    var selectFoorm_colaborador = {
+        "accion": "getcolumnAll",
+        "column": "col_dni"
+    }
+    fetchData(selectFoorm_colaborador);
+});
+btnInsert.addEventListener("click", () => {
+    var valInsert_estado_Usuario = '';
+    var valInsert_nombrecolaborador_Usuario = iptInsert_nombrecolaborador_Usuario.value();
+    var valInsert_dnicolaborador_Usuario = iptInsert_dnicolaborador_Usuario.value();
+    var valInsert_nombre_Usuario = iptInsert_nombre_Usuario.value();
+    var valInsert_clave_Usuario = iptInsert_clave_Usuario.value();
+    var valInsert_correo_Usuario = iptInsert_correo_Usuario.value();
+    if (iptInsert_estado_Usuario.checked) {
+        valInsert_estado_Usuario = true;
+    } else {
+        valInsert_estado_Usuario = false;
+    }
+
+});
+
+
+const fetchData = async (request) => {
+    const body = new FormData();
+    body.append("data", JSON.stringify(request));
+    const res = await fetch("./../../../controllers/controllerColaboradorList.php", {
+        method: "POST",
+        body
+    });
+    const data = await res.json() //await JSON.parse(returned);
+}
+
+
 /* const btnBuscar;
 const iptColaborador; */
 
