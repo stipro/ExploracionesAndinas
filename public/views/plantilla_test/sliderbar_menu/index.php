108cd8e53110ef6630cba5cf59fb2efc8df63ac3
Laptop Asus <stipro150197@gmail.com>
2022-04-25 08:24:46 -0500

    agregando cambios en sliderbar_menu

---
+++ public/views/plantilla_test/sliderbar_menu/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exploraciones Andinas</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="./icomoon/style.css">
+    <!--Nifty Stylesheet [ REQUIRED ]-->
+    <link href=".\..\..\..\..\css\nifty.min.css" rel="stylesheet">
+    <!--Nifty Premium Icon [ DEMONSTRATION ]-->
+    <link href=".\..\..\..\..\css\demo\nifty-demo-icons.min.css" rel="stylesheet">
+    <!--Font Awesome [ OPTIONAL ]-->
+    <link href=".\..\..\..\..\plugins\font-awesome\css\font-awesome.min.css" rel="stylesheet">
+    <link href='./base.css' rel='stylesheet'>
+</head>
+<body class='snippet-body'>
+<body id="body-pd">
+    <header class="header" id="header">
+        <button id="header-toggle" class="header_toggle nav-toggler header__btn btn btn-icon btn-sm">
+            <i class='fa fa-navicon'></i>
+        </button>
+        <!--<div class="header_toggle"> <i class='fa fa-navicon' id="header-toggle"></i> </div>-->
+        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div>
+                <a href="#" class="nav_logo">
+                    <img src="./icons/logo_ico.svg" alt="Logo" style="width: 28px;">
+                    <span class="nav_logo-name">Exploraciones A.</span>
+                </a>
+                <div class="nav_list">
+                    <a href="#" class="nav_link active">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="nav_icon icon-mina-de-oro">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                            <polyline points="9 22 9 12 15 12 15 22" />
+                        </svg>
+                        <!--
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                            <polyline points="9 22 9 12 15 12 15 22" />
+                        </svg>-->
+                        <span class="nav_name">Incio</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class='nav_icon bi bi-clipboard2-check'></i>
+                        <span class="nav_name">Tareo</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class='nav_icon icon-mina-de-oro'></i>
+                        <span class="nav_name">Mina</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class='nav_icon icon-recursos-humanos2'></i>
+                        <span class="nav_name">Recursos H.</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class='nav_icon icon-recursos-humanos'></i>
+                        <span class="nav_name">Admin. Usuarios</span>
+                    </a>
+                </div>
+            </div>
+            <a href="#" class="nav_link">
+                <i class='nav_icon icon-exit'></i>
+                <span class="nav_name">Desconectar</span>
+            </a>
+        </nav>
+    </div>
+    <!--Container Main start-->
+    <div class="height-100 bg-light">
+        <h4>Main Components</h4>
+    </div>
+    <!--Container Main end-->
+
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'></script>
+    <script type='text/javascript' src='base.js'></script>
+</body>
+</html>
